0ad6f74a029f0d7d27576ba7c76a4e137a4380a8
sporkupine <coffeechief25@gmail.com>
2021-12-10 17:49:34 -0600

    login works (logs to console)

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface AuthResponseData {
   idToken: string;
@@ -14,7 +15,6 @@ interface AuthResponseData {
 @Injectable({
   providedIn: 'root',
 })
-
 export class AuthService {
   isLoggedIn: boolean = false;
 
@@ -23,11 +23,69 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   onSignup(email: string, password: string) {
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.firebaseKey, {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    })
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+          this.firebaseKey,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorResponse) => {
+          let errorMessage = 'An unkown error occurred.';
+          if (!errorResponse.error || !errorResponse.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorResponse.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage =
+                'There is already an account associated with this email address. Did you mean to sign in?';
+            case 'EMAIL_NOT_FOUND':
+              errorMessage =
+                'There is no account associated with this email address. Did you mean to sign up?';
+            case 'INVALID_PASSWORD':
+              errorMessage =
+                'The password you entered was incorrect for this account. Please contact an administrator if you have forgotten your password.';
+          }
+          return throwError(errorMessage);
+        })
+      );
+  }
+
+  onSignin(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+          this.firebaseKey,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorResponse) => {
+          let errorMessage = 'An unkown error occurred.';
+          if (!errorResponse.error || !errorResponse.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorResponse.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage =
+                'There is already an account associated with this email address. Did you mean to sign in?';
+            case 'EMAIL_NOT_FOUND':
+              errorMessage =
+                'There is no account associated with this email address. Did you mean to sign up?';
+            case 'INVALID_PASSWORD':
+              errorMessage =
+                'The password you entered was incorrect for this account. Please contact an administrator if you have forgotten your password.';
+          }
+          return throwError(errorMessage);
+        })
+      );
   }
 }
 
