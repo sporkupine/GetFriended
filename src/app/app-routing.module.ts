bde7a03050bf7fb6365973c737e3c237ccaf3226
sporkupine <coffeechief25@gmail.com>
2021-12-09 18:06:56 -0600

    Auth link in navbar works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
-import { PostsComponent } from './posts/posts.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SuggestedFriendsComponent } from './suggested-friends/suggested-friends.component';
 
