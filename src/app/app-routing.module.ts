51835546b818469816e2b4e74bc78378193295ae
sporkupine <coffeechief25@gmail.com>
2021-12-10 22:05:20 -0600

    auth-guard works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SuggestedFriendsComponent } from './suggested-friends/suggested-friends.component';
@@ -8,14 +9,18 @@ import { SuggestedFriendsComponent } from './suggested-friends/suggested-friends
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'suggested-friends', component: SuggestedFriendsComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'auth', component: AuthComponent},
-  { path: 'user/:id', component: ProfileComponent }
+  {
+    path: 'suggested-friends',
+    component: SuggestedFriendsComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'auth', component: AuthComponent },
+  { path: 'user/:id', component: ProfileComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
