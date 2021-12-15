d4981662a6191d28cb924983ca3f4596a3568f2e
sporkupine <coffeechief25@gmail.com>
2021-12-15 17:37:43 -0600

    added activeUser and getUserById

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../shared/user.model';
 import { UserService } from '../shared/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  id: number;
+  activeUser: User;
 
-
-  constructor(private userService: UserService) {
-    this.userService.getUsers()
-  }
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.userService.getUsers()
-  }
+    this.userService.getUsers();
 
+    this.id = this.route.snapshot.params['id'];
+
+  }
 }
