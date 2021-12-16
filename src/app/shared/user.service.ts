d92899934e71c5e7e05c96d5d6501bbf53e54926
sporkupine <coffeechief25@gmail.com>
2021-12-15 23:20:34 -0600

    users now have dummy posts

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -26,15 +26,40 @@ export class UserService {
             'velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus',
           date: new Date(2021, 3, 23, 8, 51),
         },
+        {
+          name: "You Want Some Dummy Text? Well I'll Give Ya Some Dummy Text!",
+          content:
+            'arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt arcu non sodales neque sodales ut etiam sit amet nisl purus in mollis nunc sed id semper risus in hendrerit gravida rutrum quisque non tellus orci ac auctor augue mauris augue neque',
+          date: new Date(2020, 11, 16, 12, 14, 12),
+        },
       ],
     },
     {
       id: 1,
-      name: 'TheUltimateWarrior@hotmail.com',
+      name: 'The Ultimate Warrior',
       imagePath:
         'https://www.biography.com/.image/t_share/MTgwNzg0MTk2OTQ2NTY4NTUy/ultimate-warrior-03.jpg',
-      bio: 'I like to Gorilla Press my enemis for Breakfast',
-      posts: [],
+      bio: 'I like to Gorilla Press my enemies for breakfast!',
+      posts: [
+        {
+          name: 'Steroids and YOU: Get Big, Loud, and Mean Before Spring Break!',
+          content:
+            'ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius vel pharetra vel turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam',
+          date: new Date(2019, 4, 16, 20, 11),
+        },
+        {
+          name: 'How to Safely Fall (or be thrown) Through A Table!',
+          content:
+            'suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque',
+          date: new Date(2019, 3, 19, 19, 19),
+        },
+        {
+          name: 'Everybody was Acting Except Hulk Hogan, All His Stuff Was Real.',
+          content:
+            'diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum mattis pellentesque id nibh tortor id aliquet lectus proin nibh nisl condimentum id venenatis a',
+          date: new Date(2019, 2, 13, 23, 21),
+        },
+      ],
     },
     {
       id: 2,
@@ -42,15 +67,52 @@ export class UserService {
       imagePath:
         'https://nypost.com/wp-content/uploads/sites/2/2021/09/book-of-boba-fett-key-art.jpg?quality=80&strip=all',
       bio: '....as you wish..',
-      posts: [],
+      posts: [
+        {
+          name: 'Why You Should Buy Your Kid a Boba Fett Action Figure (and NOT a Mandalorian one) This Holiday Season',
+          content:
+            'justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit amet aliquam id diam maecenas ultricies mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque',
+          date: new Date(2021, 11, 15, 14, 14),
+        },
+        {
+          name: 'My Ship Was Cooler than the Millenium Falcon and You Know It',
+          content:
+            'velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus',
+          date: new Date(2021, 10, 18, 18, 43),
+        },
+        {
+          name: "So You Finally Got That Flamethrower You've Been Asking For. Now What?",
+          content:
+            'nunc non blandit massa enim nec dui nunc mattis enim ut tellus elementum sagittis vitae et leo duis ut diam quam nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit',
+          date: new Date(2021, 3, 23, 8, 51),
+        },
+      ],
     },
     {
       id: 3,
-      name: 'MrWolfman@gmail.com',
+      name: 'The Wolf Man',
       imagePath:
         'https://i.pinimg.com/originals/4c/75/8a/4c758a89ee726431a8902e01b008dd7a.jpg',
-      bio: 'I love beef Chowmein',
-      posts: [],
+      bio: 'I love beef chow mein',
+      posts: [
+        {
+          name: "I'm from a 1941 horror movie you probably haven't seen",
+          content:
+            'ac turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas integer eget',
+          date: new Date(2021, 11, 11, 11, 51),
+        },
+        {
+          name: 'I was played by Lon Chaney, Jr.',
+          content:
+            'velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus',
+          date: new Date(2021, 10, 7, 13, 22),
+        },
+        {
+          name: 'I have the coolest profile pic on this silly website',
+          content: "...and it's not even close",
+          date: new Date(2021, 9, 18, 17, 49),
+        },
+      ],
     },
   ];
 
